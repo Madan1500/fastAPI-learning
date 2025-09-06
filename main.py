aafde16b0f19012fc1cacb73d881725df44fda23
Madan <madanmohan14072002@gmail.com>
2025-09-06 09:33:45 +0530

    query parameters in fastAPI with various validations

--- main.py
+++ main.py
@@ -1,74 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional, Annotated,List
 
 app = FastAPI()
 
+# Query Parameters and String Validations
+
 """
-This FastAPI application defines an endpoint to read items with pagination.
+This example demonstrates how to use query parameters in FastAPI with various string validations.
 
 Endpoints:
-- GET /items/
-  - Query Parameters:
-    - skip (int, optional): The number of items to skip. Default is 0.
-    - limit (int, optional): The maximum number of items to return. Default is 10.
-  - Response:
-    - JSON object containing the 'skip' and 'limit' values.
+- GET /items/:
+    - Query Parameters:
+        - q (Optional[str]): A query string that must be between 3 and 50 characters in length and can only contain alphanumeric characters and spaces.
+    - Responses:
+        - 200: A JSON object containing the query string.
 """
-
 @app.get("/items/")
-async def read_items(skip: int = 0, limit: int = 10):
-    return {"skip": skip, "limit": limit}
+async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=50, regex="^[a-zA-Z0-9 ]*$")):
+    return {"query": q}
 
-# Query parameter type conversion
+# Query - as the default value
+@app.get("/items/")
+async def read_item(q: Optional[str] = Query(None, min_length=3, max_length=50, default=None)):
+    return {"query": q}
 
-"""
-Endpoint to retrieve an item with optional query parameters.
-
-Args:
-    item_id (str): The unique identifier for the item.
-    q (str, optional): An optional query string. Defaults to None.
-    short (bool, optional): A flag to determine if the item description should be short. Defaults to False.
-
-Returns:
-    dict: A dictionary containing the item details. If 'q' is provided, it will be included in the response. If 'short' is False, a long description will be included.
-"""
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-# Multiple path and query parameters
-
-"""
-Endpoint to read a specific item owned by a user.
-
-Args:
-    user_id (int): The ID of the user.
-    item_id (str): The ID of the item.
-    q (str, optional): An optional query string. Defaults to None.
-    short (bool, optional): A flag to indicate if a short description is requested. Defaults to False.
-
-Returns:
-    dict: A dictionary containing item details including `item_id`, `owner_id`, and optionally `q` and `description`.
-"""
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
+# Add more validations using Annotated
+@app.get("/items/")
+async def read_item_(
+    q: Annotated[str | None, Query(min_length=3, max_length=50)] = None,
 ):
-    item = {"item_id": item_id, "owner_id": user_id}
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+        results.update({"q": q})
+    return results
+
+# Default values with Annotated
+@app.get("/users/")
+async def read_users(q: Annotated[str, Query(min_length=3)] = "fixedquery"):
+    results = {"users": [{"user_id": "Foo"}, {"user_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+# Query parameter list / multiple values
+@app.get("/user/")
+async def read_users(q: Annotated[List[str] | None, Query()] = None):
+    query_users = {"q": q}
+    return query_users
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
